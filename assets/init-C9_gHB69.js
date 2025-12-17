c65ce72ebc788b649b7a41b517d548d51b372850
wtaype <wtaype@users.noreply.github.com>
2025-12-17 05:18:20 +0000

    deploy: fcaa6c15f3b4fe6de237435ba7faa362871fa088

---
+++ assets/init-C9_gHB69.js
@@ -0,0 +1,1 @@
+import{p as a,r as e,t as s}from"./firebase-BK5RP8AR.js";const o={apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""},t=s(o),p=a(t),r=e(t);export{p as a,r as d};
