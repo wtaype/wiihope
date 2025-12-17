517aadbb16496f18b1e61f8dc6a3cde7dd4e2ae3
wtaype <wtaype@users.noreply.github.com>
2025-12-17 02:19:16 +0000

    deploy: 2276c6e7aae51eadf35aae618379a3c3a9c9567c

---
+++ assets/init-BO2AhrbC.js
@@ -0,0 +1,1 @@
+import{l as t,m as e,n as s}from"./firebase-f4oW8GaL.js";const o={apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""},a=s(o),n=t(a),p=e(a);export{n as a,p as d};
