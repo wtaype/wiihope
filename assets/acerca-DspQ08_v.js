1538a6821a1ebe7d3def142413bfc45ff1234eae
wtaype <wtaype@users.noreply.github.com>
2026-01-04 19:38:08 +0000

    deploy: 083886588acb4f0acd652bf2e5b42effbd06a7dc

---
+++ assets/acerca-DspQ08_v.js
@@ -0,0 +1,20 @@
+import{$ as a}from"./main-CE5givYW.js";import"./vendor-B2AETYxa.js";let r="WiiHope",o=2024,s="@wilder.taype",t="https://wtaype.github.io/",n="v16";function i(){const e=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${t}" target="_blank">${s}</a></span>
+    <span>${o} - <span class="wty">${e.getFullYear()}</span></span>
+    <span class="abw"> | ${r} ${n} | actualizado:
+    <span class="wtu">${e.toLocaleString()}</span></span>
+  </footer>
+  `}a("body").append(i());const p=`
+:root{--bgim:url("https://d35aaqx5ub95lt.cloudfront.net/images/star-pattern.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
+`;a("head").append(`<style>${p}</style> `);const d=()=>{a("#acerca").html(`
+        <h3 class='awbt'>Acerca de nosotros</h3>
+        <div class='awbc'>
+        <p>Wiihope es una aplicación diseñada para compartir y aprender más sobre la palabra de Dios. Su propósito es fortalecer la fe y ser una herramienta de protección, prosperidad y bendición.</p>
+        <p>En esta app encontrarás el Padre Nuestro, o como mi madre solía llamarlo, "el teléfono de Dios". Ella me decía que esta oración es la clave para comunicarnos con Él, ya que fue la que Jesús nos enseñó para mostrar cómo orar correctamente.</p>
+        <p>Además, hemos reunido las mejores frases de protección, salvación y seguridad en Dios para inspirarte en tu día a día. También podrás disfrutar de audios dramatizados del Nuevo Testamento en quechua, ideales para entender y transmitir el mensaje de una manera más cercana y enriquecedora. Agradecemos a <a href='https://www.faithcomesbyhearing.com/audio-bible-resources/mp3-downloads' target='_blank'>Faith Comes By Hearing</a>, la creadora de este contenido maravilloso, por permitirnos incluirlo en Wiihope. Esta app fue desarrollada por mí, Wilder Taype, con el objetivo de digitalizar estos recursos y hacerlos accesibles para más personas.</p>
+        <p>Si llegaste hasta aquí, &#161;Dios te bendiga! Y si no, también, porque su amor y cuidado están contigo siempre. &#161;Recuerda avanzar con fe y confianza!</p>
+        <p>Con amor.</p>
+        <p><strong>Wilder Taype</strong><br/>Creador de Wiihope</p>
+        </div>
+    `)};export{d as wiAcerca};
