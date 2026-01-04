678857a0fd9dabe0424f6fbe3b357f85943b3122
wtaype <wtaype@users.noreply.github.com>
2026-01-04 23:54:18 +0000

    deploy: 0a79dffbb6f26ab65e977f4ddaee93d0cb78b3c2

---
+++ assets/header-DVEEn_Vw.js
@@ -0,0 +1,10 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/wiauth-9xmJedea.js","assets/main-DS6fWfK5.js","assets/jquery-DduPepyT.js","assets/vendor-B2AETYxa.js","assets/init-DcKKffQQ.js","assets/firebase-B8KvrrVe.js","assets/widev-C5pE0C-0.js"])))=>i.map(i=>d[i]);
+import{_ as o}from"./main-DS6fWfK5.js";import{$ as e}from"./jquery-DduPepyT.js";import{Mensaje as m,getls as _,removels as u,wiSmart as p}from"./widev-C5pE0C-0.js";import"./vendor-B2AETYxa.js";p({js:[()=>o(()=>import("./wiauth-9xmJedea.js"),__vite__mapDeps([0,1,2,3,4,5,6]))]});function d(t){m?.("Bienvenido "+t.nombre),e(".wiauth").html(`
+    <div class="sesion">
+      <img src="${t.imagen||"./smile.png"}" alt="${t.nombre}"><span>${t.nombre}</span>
+    </div>
+    <button class="bt_salir"><i class="fas fa-sign-out-alt"></i> <span> Salir </span></button>
+  `)}const f=(()=>{let t=_("wiSmile");t?l(t):r();function r(){e(".wiauth").html(`
+      <button class="wibtn_auth registrar"><i class="fas fa-user-plus"></i><span>Registrar</span></button>
+      <button class="wibtn_auth login"><i class="fas fa-sign-in-alt"></i><span>Login</span></button>
+  `)}async function l(a){d(a);const{auth:n,onAuthStateChanged:s}=await o(async()=>{const{auth:i,onAuthStateChanged:c}=await import("./wiauth-9xmJedea.js");return{auth:i,onAuthStateChanged:c}},__vite__mapDeps([0,1,2,3,4,5,6]));s(n,i=>{if(!i)return u("wiSmile"),r()})}window.addEventListener("wiFresh",a=>l(a.detail)),e(document).on("click",".bt_salir",async()=>{const{auth:a,signOut:n}=await o(async()=>{const{auth:s,signOut:i}=await import("./wiauth-9xmJedea.js");return{auth:s,signOut:i}},__vite__mapDeps([0,1,2,3,4,5,6]));await n(a),location.reload(),u("wiSmile wiFlags")})})();export{f as header,d as personal};
